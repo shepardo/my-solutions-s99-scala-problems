066cb9dd9e3b55243db146a29bfc42d3e5af4086
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2021-04-24 01:01:01 -0700

    Adding solution for p21.

--- p21/p21/src/main/scala/P21.scala
+++ p21/p21/src/main/scala/P21.scala
@@ -1,3 +1,30 @@
-object Main extends App {
-  println("Hello, World!")
+package com.shepardo.p99
+
+import scala.collection.mutable.ListBuffer
+
+class P21[T] {
+
+  def insertAt(elem: T, pos: Int, l: List[T]): List[T] = {
+    val lb = new ListBuffer[T]()
+    doInsertAt(elem, 0, pos, l, lb)
+  }
+
+  private def doInsertAt(elem: T, pos: Int, targetPos: Int, l: List[T], lb: ListBuffer[T]): List[T] = {
+    l match {
+      case xi :: Nil =>
+        if (pos == targetPos) {
+          lb.addOne(elem)
+        }
+        lb.addOne(xi)
+        lb.toList
+      case xi :: xs =>
+        if (pos == targetPos) {
+          lb.addOne(elem)
+        }
+        lb.addOne(xi)
+        doInsertAt(elem, pos + 1, targetPos, xs, lb)
+      case _ =>
+        throw new IllegalArgumentException("'l' not found")
+    }
+  }
 }
