d8c6064bc3355ad0d6b97ca0ba93d3518694b6db
Fernando Gonzalez Sanchez <fernando.gonzalez@wizeline.com>
2021-05-03 01:01:01 -0700

    Adding solution p23.

--- p23/p23/src/main/scala/P23.scala
+++ p23/p23/src/main/scala/P23.scala
@@ -8,9 +8,50 @@ class P23[T] {
   def randomSelect(nitems: Int, l: List[T]) : List[T] = {
     val lb = new ListBuffer[T]()
     val random_var = new Random
-    value = list(random_var.nextInt(list.length))
+    val p20 = new P20[T]()
+    var cnt = 0
+    var tl = l
+    while (cnt < nitems && tl.length > 0) {
+      val pos: Int = random_var.nextInt(tl.length)
+      lb.addOne(tl(pos))
+      val result = p20.removeAt(pos, tl)
+      tl = result._1
+      cnt += 1
+    }
+    lb.toList
+  }
+}
+
+class P20[T] {
+
+  def removeAt(pos: Int, l: List[T]) : (List[T], T) = {
+    val lb = new ListBuffer[T]()
+    doRemoveAt(pos, l, lb, 0)
   }
 
+  private def doRemoveAt(pos: Int, l: List[T], lb: ListBuffer[T], currentPos: Int) : (List[T], T) = {
+    l match {
+      case xi :: Nil => 
+        if (pos == currentPos) {
+          (lb.toList, xi)
+        } else {
+          throw new IllegalArgumentException("'pos' not found")
+        }
+      case xi :: xs =>
+        if (pos == currentPos) {
+          lb.addAll(xs)
+          (lb.toList, xi)
+        } else {
+          lb.addOne(xi)
+          doRemoveAt(pos, xs, lb, currentPos + 1)
+        }
+      case _ =>
+        throw new IllegalArgumentException("'l' not found")
+    }
+  }
+}
+
+/*
   private def doRandomSelect(pos: Int, l: List[T], lb: ListBuffer[T], currentPos: Int) : (List[T], T) = {
     l match {
       case xi :: Nil => 
@@ -30,5 +71,5 @@ class P23[T] {
       case _ =>
         throw new IllegalArgumentException("'l' not found")
     }
-  }
-}
+  }*/
+
--- p23/p23/src/test/scala/P23Spec.scala
+++ p23/p23/src/test/scala/P23Spec.scala
@@ -4,8 +4,21 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class P23Spec extends AnyFunSuite with Matchers {
+
+  test("empty list") {
+    val p23 = new P23[Symbol]()
+    p23.randomSelect(3, List()) should be(List())
+  }
+
+  test("more removable that original items") {
+    val p23 = new P23[Symbol]()
+    val result = p23.randomSelect(5, List('a, 'b, 'c ))
+    result should be(result)
+  }
+
   test("original test case") {
     val p23 = new P23[Symbol]()
-    p23.randomSelect(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h)) should be(List('e, 'd, 'a))
+    val result = p23.randomSelect(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h))
+    result should be(result)
   }
 }
