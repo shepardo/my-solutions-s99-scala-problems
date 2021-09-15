d37c84e6dba4c257f9e92ed3a0d4f299032efe40
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-09-14 23:15:49 -0500

    Adding solution for s25.

--- p25/p25/src/main/scala/P25.scala
+++ p25/p25/src/main/scala/P25.scala
@@ -6,6 +6,55 @@ import scala.util.Random
 class P25[T] {
 
   def randomPermute(l: List[T]) : List[T] = {
-    // test
+    val p23 = new P23[T]()
+    p23.randomSelect(l.length, l)
+  }
+}
+
+class P23[T] {
+
+  def randomSelect(nitems: Int, l: List[T]) : List[T] = {
+    val lb = new ListBuffer[T]()
+    val random_var = new Random
+    val p20 = new P20[T]()
+    var cnt = 0
+    var tl = l
+    while (cnt < nitems && tl.length > 0) {
+      val pos: Int = random_var.nextInt(tl.length)
+      lb.addOne(tl(pos))
+      val result = p20.removeAt(pos, tl)
+      tl = result._1
+      cnt += 1
+    }
+    lb.toList
+  }
+}
+
+class P20[T] {
+
+  def removeAt(pos: Int, l: List[T]) : (List[T], T) = {
+    val lb = new ListBuffer[T]()
+    doRemoveAt(pos, l, lb, 0)
+  }
+
+  private def doRemoveAt(pos: Int, l: List[T], lb: ListBuffer[T], currentPos: Int) : (List[T], T) = {
+    l match {
+      case xi :: Nil =>
+        if (pos == currentPos) {
+          (lb.toList, xi)
+        } else {
+          throw new IllegalArgumentException("'pos' not found")
+        }
+      case xi :: xs =>
+        if (pos == currentPos) {
+          lb.addAll(xs)
+          (lb.toList, xi)
+        } else {
+          lb.addOne(xi)
+          doRemoveAt(pos, xs, lb, currentPos + 1)
+        }
+      case _ =>
+        throw new IllegalArgumentException("'l' not found")
+    }
   }
 }
