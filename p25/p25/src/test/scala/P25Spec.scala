693b759b64df6fd2ab2487da1656676ef029ea6a
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-09-14 23:21:54 -0500

    Adding more test cases for s25.

--- p25/p25/src/test/scala/P25Spec.scala
+++ p25/p25/src/test/scala/P25Spec.scala
@@ -10,4 +10,16 @@ class P25Spec extends AnyFunSuite with Matchers {
     val result = p25.randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
     result.foreach(println)
   }
+
+  test("empty list") {
+    val p25 = new P25[Int]()
+    val result = p25.randomPermute(List())
+    result.foreach(println)
+  }
+
+  test("another test case") {
+    val p25 = new P25[Int]()
+    val result = p25.randomPermute(List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+    result.foreach(println)
+  }
 }
