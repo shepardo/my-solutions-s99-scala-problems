6b8794dc529f5a8bbaad3bc6a0f78ae2d6be8bc5
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-09-27 23:43:50 -0500

    First checkpoint of alternative solution for P07.

--- p07/src/main/scala/P07.scala
+++ p07/src/main/scala/P07.scala
@@ -21,3 +21,15 @@ class P07[T] {
   }
 
 }
+
+// P07 (**) Flatten a nested list structure.
+//     Example:
+//     scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+//     res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+
+object P07 {
+  def flatten(ls: List[Any]): List[Any] = ls flatMap {
+    case ms: List[_] => flatten(ms)
+    case e => List(e)
+  }
+}
