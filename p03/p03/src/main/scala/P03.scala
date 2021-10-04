7a4cfa622591840d6e006e6d0a57b01b2e2be084
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-10-04 03:51:02 -0500

    Final checkpoint for alternative solutions for P02.

--- p03/p03/src/main/scala/P03.scala
+++ p03/p03/src/main/scala/P03.scala
@@ -1,6 +1,10 @@
 package com.shepardo.p99
 
-class P03[T] {
+trait P03Interface[T] {
+  def nth(idx: Int, l: List[T]) : Option[T]
+}
+
+class P03Mine[T] extends P03Interface[T] {
   def nth(idx: Int, l: List[T]) : Option[T] = {
       doNth(idx, l)
   }
@@ -25,16 +29,18 @@ class P03[T] {
 //     scala> nth(2, List(1, 1, 2, 3, 5, 8))
 //     res0: Int = 2
 
-object P03 {
+class P03Builtin[T] extends P03Interface[T] {
   // Trivial with builtins.
-  def nthBuiltin[A](n: Int, ls: List[A]): A =
-    if (n >= 0) ls(n)
-    else throw new NoSuchElementException
+  def nth(idx: Int, l: List[T]): Option[T] =
+    if (idx >= 0 && idx < l.length) Some(l(idx))
+    else None
+}
 
-  // Not that much harder without.
-  def nthRecursive[A](n: Int, ls: List[A]): A = (n, ls) match {
-    case (0, h :: _   ) => h
-    case (n, _ :: tail) => nthRecursive(n - 1, tail)
-    case (_, Nil      ) => throw new NoSuchElementException
+class P03Recursive[T] extends P03Interface[T] {
+// Not that much harder without.
+  def nth(n: Int, ls: List[T]): Option[T] = (n, ls) match {
+    case (0, h :: _   ) => Some(h)
+    case (n, _ :: tail) => nth(n - 1, tail)
+    case (_, Nil      ) => None
   }
 }
--- p03/p03/src/test/scala/P03Spec.scala
+++ p03/p03/src/test/scala/P03Spec.scala
@@ -5,20 +5,29 @@ import org.scalatest.matchers.should.Matchers
 
 class P03Test extends AnyFunSuite with Matchers {
 
-  val p03 = new P03[Int]()
+  def createGenerators[T] = { 
+    val gens = Array(new P03Mine[T](), new P03Builtin[T](), new P03Recursive[T]())
+    for(x <- gens) yield x
+  }
 
   test("that returns element for single element's list") {
-    p03.nth(0, List(3)) should equal(Some(3))
-    p03.nth(2, List(3)) should equal(None)
+    for (p03 <- createGenerators[Int]) {
+      p03.nth(0, List(3)) should equal(Some(3))
+      p03.nth(2, List(3)) should equal(None)
+    }
   }
 
   test("that returns none for empty list") {
-    p03.nth(0, List()) should be(None)
-    p03.nth(7, List()) should be(None)
+    for (p03 <- createGenerators[Int]) {
+      p03.nth(0, List()) should be(None)
+      p03.nth(7, List()) should be(None)
+    }
   }
 
   test("that return the nth element") {
-    p03.nth(2, List(1, 1, 2, 3, 5, 8)) should be(Some(2))
-    p03.nth(4, List(1, 1, 2, 3, 5, 8)) should be(Some(5))
+    for (p03 <- createGenerators[Int]) {
+      p03.nth(2, List(1, 1, 2, 3, 5, 8)) should be(Some(2))
+      p03.nth(4, List(1, 1, 2, 3, 5, 8)) should be(Some(5))
+    }
   }
 }
