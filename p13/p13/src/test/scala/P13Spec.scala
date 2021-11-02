98abc9efabc3f19ebbfaca17d49c7cb4dda4add9
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-11-02 01:04:40 -0600

    Complete version of P13.

--- p13/p13/src/test/scala/P13Spec.scala
+++ p13/p13/src/test/scala/P13Spec.scala
@@ -10,4 +10,15 @@ class P13Test extends AnyFunSuite with Matchers {
     p13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should 
       be(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
   }
+
+  test("test with empty list") {
+    val p13 = new P13[Int]()
+    p13.encodeDirect(List()) should be(List())
+  }
+
+  test("single number test case") {
+    val p13 = new P13[Symbol]()
+    p13.encodeDirect(List('a, 'a, 'a, 'a, 'a, 'a, 'a)) should 
+      be(List((7, 'a)))
+  }
 }
