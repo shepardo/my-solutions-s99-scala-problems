8474950c526c26f6362360079f2f24dd19aaa365
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-11-02 01:13:27 -0600

    Final checkpoint of adding alternative solution for P13.

--- p13/p13/src/main/scala/P13.scala
+++ p13/p13/src/main/scala/P13.scala
@@ -2,7 +2,11 @@ package com.shepardo.p99
 
 import scala.collection.mutable.ListBuffer
 
-class P13[T] {
+trait P013Interface[T] {
+  def encodeDirect(ls: List[T]): List[(Int, T)]
+}
+
+class P13Mine[T] extends P013Interface[T] {
 
   def encodeDirect(ls: List[T]): List[(Int, T)] = {
     val lb = ListBuffer[(Int, T)]()
@@ -37,4 +41,24 @@ class P13[T] {
       case _ =>
     }
   }
+}
+
+
+// P13 (**) Run-length encoding of a list (direct solution).
+//     Implement the so-called run-length encoding data compression method
+//     directly.  I.e. don't use other methods you've written (like P09's
+//     pack); do all the work directly.
+//
+//     Example:
+//     scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+//     res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+
+class P13Functional[T] extends P013Interface[T] {
+  // This is basically a modification of P09.
+  def encodeDirect(ls: List[T]): List[(Int, T)] =
+    if (ls.isEmpty) Nil
+    else {
+      val (packed, next) = ls span { _ == ls.head }
+      (packed.length, packed.head) :: encodeDirect(next)
+    }
 }
--- p13/p13/src/test/scala/P13Spec.scala
+++ p13/p13/src/test/scala/P13Spec.scala
@@ -5,39 +5,28 @@ import org.scalatest.matchers.should.Matchers
 
 class P13Test extends AnyFunSuite with Matchers {
 
+ def createGenerators[T] = { 
+    val gens = Array(new P13Mine[T](), new P13Functional[T]())
+    for( x <- gens) yield x
+  }
+
   test("test original test case") {
-    val p13 = new P13[Symbol]()
-    p13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should 
-      be(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+    for (p13 <- createGenerators[Symbol]) {
+      p13.encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should 
+        be(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+    }
   }
 
   test("test with empty list") {
-    val p13 = new P13[Int]()
-    p13.encodeDirect(List()) should be(List())
+    for (p13 <- createGenerators[Int]) {
+      p13.encodeDirect(List()) should be(List())
+    }
   }
 
   test("single number test case") {
-    val p13 = new P13[Symbol]()
-    p13.encodeDirect(List('a, 'a, 'a, 'a, 'a, 'a, 'a)) should 
-      be(List((7, 'a)))
-  }
-}
-
-// P13 (**) Run-length encoding of a list (direct solution).
-//     Implement the so-called run-length encoding data compression method
-//     directly.  I.e. don't use other methods you've written (like P09's
-//     pack); do all the work directly.
-//
-//     Example:
-//     scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-//     res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
-
-object P13 {
-  // This is basically a modification of P09.
-  def encodeDirect[A](ls: List[A]): List[(Int, A)] =
-    if (ls.isEmpty) Nil
-    else {
-      val (packed, next) = ls span { _ == ls.head }
-      (packed.length, packed.head) :: encodeDirect(next)
+    for (p13 <- createGenerators[Symbol]) {
+      p13.encodeDirect(List('a, 'a, 'a, 'a, 'a, 'a, 'a)) should 
+        be(List((7, 'a)))
     }
+  }
 }
