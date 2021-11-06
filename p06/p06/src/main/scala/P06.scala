d96d0de1d36747a35b2e9925efdc9ad164d6ff9f
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-11-05 22:57:14 -0600

    Adding alternative solutions for P06.

--- p06/p06/src/main/scala/P06.scala
+++ p06/p06/src/main/scala/P06.scala
@@ -1,6 +1,10 @@
 package com.shepardo.p99
 
-class P06[T] {
+trait P06Interface[T] {
+  def isPalindrome(l: List[T]) : Boolean
+}
+
+class P06Mine[T] extends P06Interface[T] {
   def isPalindrome(l: List[T]) : Boolean = {
     l match {
       case x :: Nil => true
@@ -12,33 +16,21 @@ class P06[T] {
         else
           isPalindrome(init)
     }
-      //val lb = new ListBuffer[T]()
-      //doReverse(l, lb).toList
   }
-
-/*
-  private def doReverse(l: List[T], lb: ListBuffer[T]) : ListBuffer[T] = {
-    l match {
-      case x :: Nil => lb :+ x
-      case x :: xs => doReverse(xs, lb) :+ x
-      case _ => lb
-    }
-  }*/
 }
-
 
 // P06 (*) Find out whether a list is a palindrome.
 //     Example:
 //     scala> isPalindrome(List(1, 2, 3, 2, 1))
 //     res0: Boolean = true
 
-object P06 {
+class P06Builtin[T] extends P06Interface[T] {
   // In theory, we could be slightly more efficient than this.  This approach
   // traverses the list twice: once to reverse it, and once to check equality.
   // Technically, we only need to check the first half of the list for equality
   // with the first half of the reversed list.  The code to do that more
   // efficiently than this implementation is much more complicated, so we'll
   // leave things with this clear and concise implementation.
-  def isPalindrome[A](ls: List[A]): Boolean = ls == ls.reverse
+  def isPalindrome(ls: List[T]): Boolean = ls == ls.reverse
 }
 
--- p06/p06/src/test/scala/P06Spec.scala
+++ p06/p06/src/test/scala/P06Spec.scala
@@ -5,17 +5,26 @@ import org.scalatest.matchers.should.Matchers
 
 class P06Test extends AnyFunSuite with Matchers {
 
-  val p06 = new P06[Int]()
+  def createGenerators[T] = {
+    val gens = Array(new P06Mine[T](), new P06Builtin[T]())
+    for(x <- gens) yield x
+  }
 
   test("that returns the palindrome of a single item list") {
-    p06.isPalindrome(List(3)) should be(true)
+    for (p06 <- createGenerators[Int]) {
+      p06.isPalindrome(List(3)) should be(true)
+    }
   }
 
   test("that returns the palindrome of an empty list") {
-    p06.isPalindrome(List()) should be(true)
+    for (p06 <- createGenerators[Int]) {
+      p06.isPalindrome(List()) should be(true)
+    }
   }
 
   test("that returns the reverse of a list with several items") {
-    p06.isPalindrome(List(1, 2, 3, 2, 1)) should be(true)
+    for (p06 <- createGenerators[Int]) {
+      p06.isPalindrome(List(1, 2, 3, 2, 1)) should be(true)
+    }
   }
 }
