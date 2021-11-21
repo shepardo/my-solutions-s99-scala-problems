f34800ede7606fab62f2e542a53cd599d92be2e1
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-11-20 23:57:26 -0600

    Adding first checkpoint for alternative solutions for P23.

--- p23/p23/src/main/scala/P23.scala
+++ p23/p23/src/main/scala/P23.scala
@@ -51,25 +51,33 @@ class P20[T] {
   }
 }
 
-/*
-  private def doRandomSelect(pos: Int, l: List[T], lb: ListBuffer[T], currentPos: Int) : (List[T], T) = {
-    l match {
-      case xi :: Nil => 
-        if (pos == currentPos) {
-          (lb.toList, xi)
-        } else {
-          throw new IllegalArgumentException("'pos' not found")
-        }
-      case xi :: xs =>
-        if (pos == currentPos) {
-          lb.addAll(xs)
-          (lb.toList, xi)
-        } else {
-          lb.addOne(xi)
-          doRandomSelect(pos, xs, lb, currentPos + 1)
-        }
-      case _ =>
-        throw new IllegalArgumentException("'l' not found")
+// P23 (**) Extract a given number of randomly selected elements from a list.
+//     Example:
+//     scala> randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+//     res0: List[Symbol] = List('e, 'd, 'a)
+//
+//     Hint: Use the answer to P20.
+
+object P23 {
+  import P20.removeAt
+
+  def randomSelect1[A](n: Int, ls: List[A]): List[A] =
+    if (n <= 0) Nil
+    else {
+      val (rest, e) = removeAt((new util.Random).nextInt(ls.length), ls)
+      e :: randomSelect1(n - 1, rest)
     }
-  }*/
+
+  // It can be expensive to create a new Random instance every time, so let's
+  // only do it once.
+  def randomSelect[A](n: Int, ls: List[A]): List[A] = {
+    def randomSelectR(n: Int, ls: List[A], r: util.Random): List[A] =
+      if (n <= 0) Nil
+      else {
+        val (rest, e) = removeAt(r.nextInt(ls.length), ls)
+        e :: randomSelectR(n - 1, rest, r)
+      }
+    randomSelectR(n, ls, new util.Random)
+  }
+}
 
