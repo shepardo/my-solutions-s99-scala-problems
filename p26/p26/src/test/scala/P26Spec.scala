e6768861366eb13b05de305a12c7ded5a5060abf
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-12-03 01:01:01 -0700

    Adding checkpoint for alternative solutions for P26.

--- p26/p26/src/test/scala/P26Spec.scala
+++ p26/p26/src/test/scala/P26Spec.scala
@@ -8,8 +8,18 @@ class P26Spec extends AnyFunSuite with Matchers {
     test("original test case") {
         val p26 = new P26[Symbol]()
         val result = p26.combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
-        // TODO
-        result should be(List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), List('a, 'b, 'f), List('a, 'c, 'd), List('a, 'c, 'e), List('a, 'c, 'f), List('a) ))
+        result should be(List(
+            List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), List('a, 'b, 'f), 
+            List('a, 'c, 'd), List('a, 'c, 'e), List('a, 'c, 'f),
+            List('a, 'd, 'e), List('a, 'd, 'f),
+            List('a, 'e, 'f),
+            List('b, 'c, 'd), List('b 'c, 'e), List('b, 'c, 'f),
+            List('b, 'd, 'e), List('b 'd, 'f),
+            List('b, 'e, 'f),
+            List('c, 'd, 'e), List('c, 'd, 'f), 
+            List('c, 'e, 'f),
+            List('d, 'e, 'f)
+            ))
     }
 
     test("original test case, combination of 1") {
