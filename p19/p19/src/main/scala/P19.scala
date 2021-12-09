77beabbc6143a3f4cabb338ead110f86fa22f507
Fernando Gonzalez Sanchez <fer.gonzalez.sanchez@gmail.com>
2021-12-08 21:39:51 -0600

    Adding final checkpoint of alternative solutions for p19.

--- p19/p19/src/main/scala/P19.scala
+++ p19/p19/src/main/scala/P19.scala
@@ -2,7 +2,11 @@ package com.shepardo.p99
 
 import scala.collection.mutable.ListBuffer
 
-class P19[T] {
+trait P19Interface[T] {
+  def rotate(pos: Int, l: List[T]): List[T]
+}
+
+class P19Mine[T] extends P19Interface[T] {
 
   def rotate(pos: Int, l: List[T]): List[T] = {
     val indexes = l.zipWithIndex;
@@ -45,10 +49,10 @@ class P19[T] {
 //     scala> rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
 //     res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
 
-object P19 {
-  def rotate[A](n: Int, ls: List[A]): List[A] = {
-    val nBounded = if (ls.isEmpty) 0 else n % ls.length
-    if (nBounded < 0) rotate(nBounded + ls.length, ls)
-    else (ls drop nBounded) ::: (ls take nBounded)
+class P19Functional[T] extends P19Interface[T] {
+  def rotate(n: Int, l: List[T]): List[T] = {
+    val nBounded = if (l.isEmpty) 0 else n % l.length
+    if (nBounded < 0) rotate(nBounded + l.length, l)
+    else (l drop nBounded) ::: (l take nBounded)
   }
 }
--- p19/p19/src/test/scala/P19Spec.scala
+++ p19/p19/src/test/scala/P19Spec.scala
@@ -5,33 +5,45 @@ import org.scalatest.matchers.should.Matchers
 
 class P19Spec extends AnyFunSuite with Matchers {
 
+
+  def createGenerators[T] = {
+    val gens = Array(new P19Mine[T](), new P19Functional[T]())
+    for(x <- gens) yield x
+  }
+
   test("empty list") {
-    val p19 = new P19[Int]()
-    p19.rotate(3, List()) should be (List())
+    for (p19 <- createGenerators[Int]) {
+      p19.rotate(3, List()) should be (List())
+    }
   }
 
   test("empty list 2nd") {
-    val p19 = new P19[Int]()
-    p19.rotate(-4, List()) should be(List())
+    for (p19 <- createGenerators[Int]) {
+      p19.rotate(-4, List()) should be(List())
+    }
   }
 
   test("List with single element") {
-    val p19 = new P19[Int]()
-    p19.rotate(5, List(1)) should be(List(1))
+    for (p19 <- createGenerators[Int]) {
+      p19.rotate(5, List(1)) should be(List(1))
+    }
   }
 
   test("List with single element 2nd") {
-    val p19 = new P19[Int]()
-    p19.rotate(-7, List(3)) should be(List(3))
+    for (p19 <- createGenerators[Int]) {
+      p19.rotate(-7, List(3)) should be(List(3))
+    }
   }
 
   test("original test case") {
-    val p19 = new P19[Symbol]()
-    p19.rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should be(List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c))
+    for (p19 <- createGenerators[Symbol]) {
+      p19.rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should be(List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c))
+    }
   }
 
   test("original test case 2nd") {
-    val p19 = new P19[Symbol]()
-    p19.rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should be(List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i))
+    for (p19 <- createGenerators[Symbol]) {
+      p19.rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) should be(List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i))
+    }
   }
 }
